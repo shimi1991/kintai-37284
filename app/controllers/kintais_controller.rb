25ea8a54b9000b9abbca1e2eb2cac7f2d7e23bda
shimi1991 <yuu.9134@gmail.com>
2022-08-01 12:18:26 +0900

    コメント編集

--- app/controllers/kintais_controller.rb
+++ app/controllers/kintais_controller.rb
@@ -12,8 +12,8 @@ class KintaisController < ApplicationController
       when "出勤" ; session[:start_time] = Time.now
       when "退勤" ; session[:end_time] = Time.now
     end
-    # 出勤、退勤ボタンを押した時の処理、出勤ボタンのみでは一時保存で出勤と退勤が揃うと保存される
-    if session[:start_time] != nil && session[:end_time] != nil
+    # 出勤、退勤ボタンを押した時の処理
+    if session[:start_time] != nil && session[:end_time] != nil #出勤ボタンのみでは一時保存され、出勤と退勤が揃うと保存される
       @kintai[:start_time] = session[:start_time]
       @kintai[:end_time] = session[:end_time]
       session[:over_time] = (@kintai[:end_time] - @kintai[:start_time]).floor / 3600 - 8
